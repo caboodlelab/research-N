898b84dbf8bfd6040ec1a72da78778aaa4d67a69
Nitin Dolli <nitin.dolli@gmail.com>
2017-07-20 22:19:20 +0800

    config updated

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var config = module.exports = {};
 
-// // App configuration
-// config.env = 'dev';
-// // dev
-// config.port = 8000;
-//
-config.apiUrl = 'https://1bea6fa8.ngrok.io/api/v1/test';
+// App configuration
+config.env = 'dev';
+// dev
+config.port = (process.env.PORT || 8000);
+
+config.apiUrl = 'https://0cf9c744.ngrok.io/add-client';
