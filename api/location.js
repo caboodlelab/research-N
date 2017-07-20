56c1e9093fc1efab061416b4fd33c703d7c824b2
Nitin Dolli <nitin.dolli@gmail.com>
2017-07-20 22:07:12 +0800

    api response debugging

--- api/location.js
+++ api/location.js
@@ -12,7 +12,7 @@ const email = {
     request({
       method: 'POST',
       uri: 'https://1bea6fa8.ngrok.io/api/v1/test',
-      form: req.body,
+      body: req.body,
       json: true
     }, function (error, response, body) {
       if (error) {
